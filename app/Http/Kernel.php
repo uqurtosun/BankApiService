84289f79a3bffe942b3c6322b1e3db0b2bbc6de3
Ugur Tosun <uqurtosun@gmail.com>
2022-10-10 06:43:33 +0100

    Target Fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'LoginControl'=>\App\HttP\Middleware\LoginControl::class,
-        'LoginAuthControl'=>\App\HttP\Middleware\LoginAuthControl::class,
+        'LoginControl'=>\App\Http\Middleware\LoginControl::class,
+        'LoginAuthControl'=>\App\Http\Middleware\LoginAuthControl::class,
     ];
 }
